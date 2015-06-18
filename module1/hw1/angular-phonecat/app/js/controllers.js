7e24fc664554dfd34967a00649843b919e38ce02
Pavlo Shenhofer <pavlo_shenhofer@epam.com>
2015-06-18 15:47:36 +0300

    [hw1] added promise to controller

--- module1/hw1/angular-phonecat/app/js/controllers.js
+++ module1/hw1/angular-phonecat/app/js/controllers.js
@@ -4,47 +4,60 @@
 
 var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone','localStorageService',
-  function($scope, Phone, localStorageService,LATEST_PHONES) {
-      //paging
-      $scope.totalRecordsCount = 0;
-      $scope.pageSize = 5;
-      $scope.currentPage = 1;
-      $scope.lastPhones = localStorageService.getItem(LATEST_PHONES);
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$q', 'Phone', 'localStorageService',
+    function ($scope, $q, Phone, localStorageService, LATEST_PHONES) {
+        //paging
+        $scope.totalRecordsCount = 0;
+        $scope.pageSize = 5;
+        $scope.currentPage = 1;
+        $scope.lastPhones = localStorageService.getItem(LATEST_PHONES);
 
-      init();
-      function init(){
-          getPhones();
-      }
+        init();
+        function init() {
+            setPhoneToScope();
+        }
 
-      function getPhones(){
-          var sizePage = $scope.pageSize;
-          var currPage = $scope.currentPage;
-          Phone.query({},function(res){
-              var phones = res.slice(((currPage-1) * sizePage ), (currPage * sizePage));
-              $scope.phones = phones;
-              $scope.totalRecordsCount =  res.length;
-          });
-      };
+        function getPhoneFromServer() {
+            var deferred = $q.defer();
+            Phone.query({}, function (res) {
+                if (res) {
+                    deferred.resolve(res);
+                }
+                else {
+                    deferred.reject("error");
+                }
+            });
+            return deferred.promise;
+        }
 
-      $scope.pageChanged = function (page) {
-          $scope.currentPage = page;
-          getPhones($scope.pageSize, $scope.currentPage);
-      };
+        function setPhoneToScope() {
+            var sizePage = $scope.pageSize;
+            var currPage = $scope.currentPage;
+            getPhoneFromServer().then(function (res) {
+                var phones = res.slice(((currPage - 1) * sizePage ), (currPage * sizePage));
+                $scope.phones = phones;
+                $scope.totalRecordsCount = res.length;
+            });
+        };
 
-      $scope.orderProp = 'age';
-  }]);
+        $scope.pageChanged = function (page) {
+            $scope.currentPage = page;
+            getPhones($scope.pageSize, $scope.currentPage);
+        };
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone','latestPhoneService',
-    function($scope, $routeParams, Phone, latestPhoneService, LATEST_PHONES) {
-        var phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+        $scope.orderProp = 'age';
+    }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone', 'latestPhoneService',
+    function ($scope, $routeParams, Phone, latestPhoneService, LATEST_PHONES) {
+        var phone = Phone.get({phoneId: $routeParams.phoneId}, function (phone) {
             $scope.mainImageUrl = phone.images[0];
             latestPhoneService.addLastPhone(phone);
         });
 
         $scope.phone = phone;
 
-        $scope.setImage = function(imageUrl) {
+        $scope.setImage = function (imageUrl) {
             $scope.mainImageUrl = imageUrl;
         };
 
